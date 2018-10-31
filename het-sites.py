4409c2633da5ec926a4746102289fc5078671556
Talitha Veith <talitha.veith@v-online.me>
2018-10-31 17:32:09 +0100

    renaming het-sites.py

---
+++ het-sites.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from __future__ import print_function, division
+
+from collections import Counter
+from dark.fasta import FastaReads
+from math import log10
+import argparse
+
+#parser = argparse.ArgumentParser()
+
+#parser.add_argument("--hom", type=int,
+                   # help="fraction of one base at a site at which a site is considered to be homogeneious.")
+
+reads = list(FastaReads('Alignment-CavV-strains.fasta'))
+
+if len(set(len(read) for read in reads)) != 1:
+    raise ValueError('Not all read lengths are the same.....')
+
+length = len(reads[0])
+width = int(log10(length)) + 1
+total_heterogeneious = 0
+
+for index in range(length):
+    counts = Counter()
+    for read in reads:
+        counts[read.sequence[index]] += 1
+
+    if len(counts) > 1:
+        # The site is not homogenous = more than one nucleotide present.
+        print('Site %*d has nucleotides %s' % (width, index + 1, counts))
+        total_heterogeneious += 1
+
+print('The total of heterogeneious sites is %i.' % (total_heterogeneious))
+
+# add a parsing function, when to include a site as heterogeneious
