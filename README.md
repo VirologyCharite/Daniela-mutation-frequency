62b57c5d0cb6094204de1fa38e77e74c17761920
Talitha Veith <talitha.veith@v-online.me>
2018-11-07 14:25:40 +0100

    Cleaned up.Cleaned up.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This code takes fasta reads of equal length and compares them to give:
 
 1) The heterogeneous sites (using `heterogeneousSites` function) 
-2) A count of the 12 possible mutations in all sequences given, in relation to a reference sequence (using `compareToRef`function)
+2) A count of the 12 possible mutations in all sequences given, in relation to a reference sequence (using `compareToRef` function)
--- analyse_heterogeneous_sequences.py
+++ analyse_heterogeneous_sequences.py
@@ -4,18 +4,21 @@ from dark.fasta import FastaReads
 from math import log10
 from heterogeneous_sequences import heterogeneousSites, compareToRef
 import argparse
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--homogenFraction", type=float, default=1.0,
                     help="If the fraction of the most common nucleotide at a site is at least this value, the site will be considered homogenous.")
 parser.add_argument("--reference",
                     help="Filename of the reference fasta sequence.")
+parser.add_argument("--reads", default=sys.stdin,
+                    help="Filename of the reference fasta sequence.")
 args = parser.parse_args()
 
 if 0 < args.homogenFraction < 1:
     raise ValueError('--homogenFraction needs to be between 0 and 1.')
 
-reads = list(FastaReads('Alignment-CavV-strains.fasta'))
+reads = list(FastaReads(args.reads))
 
 if len(set(len(read) for read in reads)) != 1:
     raise ValueError('Not all read lengths are the same.....')
@@ -29,7 +32,7 @@ if args.reference is not None:
     # C{defaultdict} containing mutation counts.
     totalmutations = sum(mutationsdict.values())
     mutationsSorted = sorted(zip(mutationsdict.values(), mutationsdict.keys()), reverse=True)
-    # Mutation counts sorted by count.
+    # Mutationsdict sorted by count.
 
 print('Total count of mutations in sequences as compared to the reference sequence:')
 for mutation in mutationsSorted:
